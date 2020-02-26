95b6054c106077eca9c2daa079db49dee493bd03
JamesShoaf <james.s.shoaf@gmail.com>
2020-02-25 17:33:48 -0800

    partial implementation of basicChaining.js

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -10,11 +10,27 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
+var request = require('request');
+var readFile = Promise.promisify(fs.readFile);
+var requestGet = Promise.promisify(request.get);
+var writeFile = Promise.promisify(fs.writeFile);
 
 
-
-var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
+var fetchProfileAndWriteToFile = (readFilePath, writeFilePath) => {
   // TODO
+  var fetched = readFile(readFilePath)
+    .then((result) => {
+      var options = { //reads first line of file and appends to URL
+        url: 'https://api.github.com/users/' + result.toString().split('\n')[0],
+        headers: { 'User-Agent': 'request' },
+        json: true
+      };
+      return requestGet(options);
+    })
+    .then((result) => {
+      return writeFile(writeFilePath, result, 'utf8');
+    });
+  return fetched;
 };
 
 // Export these functions so we can test them
